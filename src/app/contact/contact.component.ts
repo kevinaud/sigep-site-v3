1514a47e3f5efff0c8c1f7058247d5091ec4b05d
Kevin Aud <kevinaud@gmail.com>
2016-11-01 23:40:18 -0500

    finished contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from './contact';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  contacts: Contact[] = [
+    {
+      name: 'Kevin Aud',
+      position: 'VP of Communications',
+      email: 'Kevin_Aud@Baylor.edu',
+      phone: '540-905-9197'
+    },
+    {
+      name: 'Tommy Kava',
+      position: 'President',
+      email: 'Tommy_Kava@Baylor.edu',
+      phone: '513-641-6285'
+    },
+    {
+      name: 'Brett Feehan',
+      position: 'VP of Recruitment',
+      email: 'Brett_Feehan@Baylor.edu',
+      phone: '818-430-8788'
+    }
+  ];
 
   constructor() { }
 
